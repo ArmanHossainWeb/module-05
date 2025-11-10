ef94f94786200e33a41d31d2c3e3944e36321546
Your Name <you@example.com>
2025-11-10 11:04:17 +0600

    object, literal & optional type

---
+++ src/object.ts
@@ -0,0 +1,16 @@
+const user: {
+     readonly firstName: string; // value => type : literal type
+    middleName?: string; // for optional type
+    lastName: string;
+    isMarried: Boolean;
+} = {
+    firstName: "Shakhawat",
+    lastName: "Arman",
+    isMarried:true
+
+}
+
+
+
+
+console.log(user)
